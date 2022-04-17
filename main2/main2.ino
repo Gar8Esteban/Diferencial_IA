13379917087396926269954aa36823b23bc7e17d
Gar8Esteban <84606270+Gar8Esteban@users.noreply.github.com>
2022-04-17 16:35:28 -0500

    pid derecho 100% configurando pid izquierdo

--- main2/main2.ino
+++ main2/main2.ino
@@ -2,13 +2,13 @@ const int pin1MD = 13;
 const int pin2MD = 12;
 const int pin1MI = 2;
 const int pin2MI = 4;// pines de los motores
-const int tiempoMuestreo = 50; //50ms
+const int tiempoMuestreo = 10; //10ms
 // caracteristicas PWM 
 #define PWM0 2
 #define PWM1 3
 #define PWM2 4
 #define PWM3 5
-#define PWM_res 10
+#define PWM_res 12
 #define PWM_freq 1000
 
 // encoders
@@ -34,11 +34,11 @@ void IRAM_ATTR isrEncI(){// funcion del encoder Izquiedo
 // Filtros:
 float radD = 0.0;
 float radFilterD = radD;
-float alphaD = 0.05;
+float alphaD = 0.01;
 
 float radI = 0.0;
 float radFilterI = radI;
-float alphaI = 0.05;
+float alphaI = 0.01;
 
 //
 
@@ -65,9 +65,11 @@ struct PID{
   float I;
   float D;
   };
-PID pidRight = {200.0, 15.0, 0.0};
+PID pidRight = {3000.0, 5.0, 100.0};//50 1250
+PID pidLeft = {1000.0, 0.0, 0.0};
 int controlRight(float);
-int entradaD = 11.0;
+int controlLeft(float);
+float entradaD = 0, entradaI = 9.5;
 void setup() {
   Serial.begin(115200);// Comunicacion serial
   pinMode(pin1MD, OUTPUT);
@@ -98,11 +100,9 @@ void setup() {
   // configuracion del timer 0 a 200ms
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 50000, true);//timer a 10ms
+  timerAlarmWrite(timer, 10000, true);//timer a 10ms
   timerAlarmEnable(timer);
 
-  ledcWrite(PWM0, entradaD);
-  ledcWrite(PWM2, 0);
 }
 
 
@@ -116,13 +116,29 @@ void loop() {
     
     radI = velRad(encI.CONT);
     radFilterI = alphaI*radI + (1.0-alphaI)*radFilterI;
-    errorRight = entradaD - radFilterD;
-    Serial.print(entradaD);
+    errorRight = abs(entradaD) - radFilterD;
+    errorLeft = abs(entradaI) - radFilterI;
+    Serial.print(abs(entradaI));
     Serial.print(",");
-    Serial.print(radFilterD);
+    Serial.print(radFilterI);
     Serial.print(",");
-    Serial.println(errorRight);
-    ledcWrite(PWM0, controlRight(errorRight));
+    Serial.println(errorLeft);
+    
+    if(entradaD > 0){
+      ledcWrite(PWM0, controlRight(errorRight));
+      ledcWrite(PWM1, 0);
+      }else{
+        ledcWrite(PWM0, 0);
+        ledcWrite(PWM1, controlRight(errorRight));
+        }
+        
+    if(entradaI > 0){
+      ledcWrite(PWM2, controlLeft(errorLeft));
+      ledcWrite(PWM3, 0);
+      }else{
+        ledcWrite(PWM2, 0);
+        ledcWrite(PWM3, controlLeft(errorLeft));
+        }
     
     encD.CONT = 0;
     encI.CONT = 0;
@@ -151,13 +167,38 @@ int controlRight(float e){
   Ekd_2 = Ekd_1;
   Ekd_1 = e;
   Ukd_1 = UK;
-  if(entradaD > 0){
-    if(UK >= 100 && UK <= 1023){
+  if(abs(entradaD) > 0){
+    if(UK >= 400 && UK <= 4095){
     return round(UK);
-    }else if(UK < 100){
-      return 100;
+    }else if(UK < 400){
+      return 400;
       }else{
-        return 1023;
+        return 4095;
+        }
+    }else{
+      return 0;
+      }
+  }
+
+float T0i = pidLeft.P + (pidLeft.D/tm) + ((pidLeft.I*tm)/2);
+float T1i = -pidLeft.P - (((2*pidLeft.D)/tm)) + ((pidLeft.I*tm)/2);
+float T2i = ((pidLeft.D)/tm);
+float Uki_1 = 0.0; //señales de control
+float Eki_1 = 0.0, Eki_2 = 0.0; // errores;
+
+int controlLeft(float e){
+  float UK;
+  UK = T0i*e + T1i*Eki_1 + T2i*Eki_2 + Uki_1;
+  Eki_2 = Eki_1;
+  Eki_1 = e;
+  Uki_1 = UK;
+  if(abs(entradaI) > 0){
+    if(UK >= 400 && UK <= 4095){
+    return round(UK);
+    }else if(UK < 400){
+      return 400;
+      }else{
+        return 4095;
         }
     }else{
       return 0;
